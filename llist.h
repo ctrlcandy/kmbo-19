df8434c253d45ae3431614317054e0b166339da9
ctrlcandy <alexandraleshanu@yandex.ru>
2020-03-08 00:42:54 +0300

    Добавила класс Node

--- llist.h
+++ llist.h
@@ -7,7 +7,21 @@
 
 class LList
 {
-	// HERE WILL BE YOUR CODE
+	private:
+
+		class Node {
+			public:
+			int  data;
+			Node* next;
+
+			Node(int data, Node* next = nullptr) {
+				this->data = data;
+				this->next = next;
+			}
+		};
+
+	Node* head;
+	size_t _size;
 
 	public:
 
@@ -16,8 +30,8 @@ class LList
 
 	void push_back(int val);         // add new value at the end:  [1 2 3 4] -> [1 2 3 4 5]
 	void push_front(int val);        // add new value at the begin [1 2 3 4] -> [5 1 2 3 4]
-	void pop_back(int val);          // remove at the end          [1 2 3 4] -> [1 2 3]
-	void pop_front(int val);         // remove at the front        [1 2 3 4] -> [2 3 4]
+	void pop_back();		 // remove at the end          [1 2 3 4] -> [1 2 3]
+	void pop_front();		 // remove at the front        [1 2 3 4] -> [2 3 4]
 	size_t size() const;             // get actual number of items [1 2 3 4] -> 4
 	int& operator[](size_t idx);     // get rw access ot specific item addressing by idx
 	int  operator[](size_t idx) const; //get read-only access
