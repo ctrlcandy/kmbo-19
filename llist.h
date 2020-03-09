bdd8adb86fe8bea91f8b318930da4f164e7f21e7
ctrlcandy <alexandraleshanu@yandex.ru>
2020-03-09 16:33:11 +0300

    Добавлен приватный метод data()

--- llist.h
+++ llist.h
@@ -11,17 +11,20 @@ class LList
 
 		class Node {
 			public:
-			int  data;
+			int data;
 			Node* next;
 
 			Node(int data, Node* next = nullptr) {
 				this->data = data;
 				this->next = next;
 			}
+			
 		};
 
-	Node* head;
-	size_t _size;
+	Node*	_head;
+	size_t	_size;
+
+	int& data(size_t idx) const;
 
 	public:
 
@@ -30,8 +33,8 @@ class LList
 
 	void push_back(int val);         // add new value at the end:  [1 2 3 4] -> [1 2 3 4 5]
 	void push_front(int val);        // add new value at the begin [1 2 3 4] -> [5 1 2 3 4]
-	void pop_back();		 // remove at the end          [1 2 3 4] -> [1 2 3]
-	void pop_front();		 // remove at the front        [1 2 3 4] -> [2 3 4]
+	void pop_back();				 // remove at the end          [1 2 3 4] -> [1 2 3]
+	void pop_front();				 // remove at the front        [1 2 3 4] -> [2 3 4]
 	size_t size() const;             // get actual number of items [1 2 3 4] -> 4
 	int& operator[](size_t idx);     // get rw access ot specific item addressing by idx
 	int  operator[](size_t idx) const; //get read-only access
