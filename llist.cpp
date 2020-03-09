fe90b2e310eba2b35bc6bccabc2676d3badf0f57
ctrlcandy <alexandraleshanu@yandex.ru>
2020-03-09 03:05:30 +0300

    Реализовала большинство функций

--- llist.cpp
+++ llist.cpp
@@ -6,7 +6,8 @@ LList::LList() {
 }
 
 LList::~LList() {
-	
+	while (_size)
+		pop_front();
 }
 
 void LList::push_back(int val) {
@@ -23,26 +24,33 @@ void LList::push_back(int val) {
 }
 
 void LList::push_front(int val) {
-
+	head = new Node(val, head);
+	_size++;
 }
 
 void LList::pop_back() {
-
+	erase_at(_size - 1);
 }
 
 void LList::pop_front() {
+	if (head == nullptr)
+		return;
 
+	Node* temp = head;
+	head = head->next;
+	delete temp;
+	_size--;
 }
 
 size_t LList::size() const{
 	return _size;
 }
 
-int& LList::operator[](size_t idx) {
+int LList::operator[](size_t idx) const {
 	Node* temp = head;
 	size_t count = 0;
 
-	while (temp->next != nullptr) {
+	while (temp != nullptr) {
 		if (count == idx)
 			return temp->data;
 
@@ -51,15 +59,38 @@ int& LList::operator[](size_t idx) {
 	}
 }
 
-int LList::operator[](size_t idx) const {
-
+int& LList::operator[](size_t idx) {
+	//...
 }
 
-void LList::erase_at(size_t idx) {
 
+void LList::erase_at(size_t idx) {
+	if (idx != 0) {
+		Node* temp = head;
+		for (size_t i = 0; i < idx - 1; i++)
+			temp = temp->next;
+
+		Node* newNode = temp->next;
+		temp->next = temp->next->next;
+		delete newNode;
+		_size--;
+	}
+	else
+		pop_front();
 }
 
 void LList::insert_at(size_t idx, int val) {
+	if (idx != 0) {
+		Node* temp = head;
+		for (size_t i = 0; i < idx - 1; i++)
+			temp = temp->next;
+
+		Node* newNode = new Node(val, temp->next);
+		temp->next = newNode;
+		_size++;
+	}
+	else
+		push_front(val);
 
 }
 
